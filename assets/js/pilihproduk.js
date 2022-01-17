126a90463bb9ea275523885d9d5c20402493747d
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-17 21:28:22 +0700

    tambah map

--- assets/js/pilihproduk.js
+++ assets/js/pilihproduk.js
@@ -1,6 +1,8 @@
 var jLength;
 var idName;
 var numberId;
+var produkDipilih;
+var databaseDipilih;
 
 
 // Function to read button
@@ -9,6 +11,7 @@ function pilihPertanian(){
     document.getElementById("produk_dipilih").innerHTML = `
     Produk Pertanian
     `;
+    databaseDipilih = 0;
 
     // Baca jumlah data
     jLenght = Object.keys(database_pertanian).length;
@@ -17,17 +20,17 @@ function pilihPertanian(){
     document.getElementById("daftar_produk").innerHTML = ``;
 
     // Buat kolom berdasarkan jumlah data
-    for(let i = 1; i <= jLenght; i++)
+    for(let i = 0; i < jLenght; i++)
     {
-        idName = "produk_" + String(i);
+        idName =  String(i);
 
         document.getElementById("daftar_produk").innerHTML += `
         <div class="col">
             <div class="card h-100 shadow">
-                <img src="${database_pertanian[i-1].img}" class="card-img-top" alt="produk">
+                <img src="${database_pertanian[i].img}" class="card-img-top" alt="produk">
                 <div class="card-body" style="text-align:center">
-                    <h5 class="card-title" style="padding-bottom:20px">${database_pertanian[i-1].nama_produk}</h5>
-                    <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="">Pilih</button>
+                    <h5 class="card-title" style="padding-bottom:20px">${database_pertanian[i].nama_produk}</h5>
+                    <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="klikProduk(this.id)">Pilih</button>
                 </div>
             </div>
         </div>
@@ -40,6 +43,7 @@ function pilihPeternakan(){
    document.getElementById("produk_dipilih").innerHTML = `
    Produk Peternakan
    `;
+   databaseDipilih = 1;
 
    // Baca jumlah data
    jLenght = Object.keys(database_peternakan).length;
@@ -48,17 +52,17 @@ function pilihPeternakan(){
    document.getElementById("daftar_produk").innerHTML = ``;
 
    // Buat kolom berdasarkan jumlah data
-   for(let i = 1; i <= jLenght; i++)
+   for(let i = 0; i < jLenght; i++)
    {
-       idName = "produk_" + String(i);
+       idName = String(i);
 
        document.getElementById("daftar_produk").innerHTML += `
        <div class="col">
            <div class="card h-100 shadow">
-               <img src="${database_peternakan[i-1].img}" class="card-img-top" alt="produk">
+               <img src="${database_peternakan[i].img}" class="card-img-top" alt="produk">
                <div class="card-body" style="text-align:center">
-                   <h5 class="card-title" style="padding-bottom:20px">${database_peternakan[i-1].nama_produk}</h5>
-                   <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="">Pilih</button>
+                   <h5 class="card-title" style="padding-bottom:20px">${database_peternakan[i].nama_produk}</h5>
+                   <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="klikProduk(this.id)">Pilih</button>
                </div>
            </div>
        </div>
@@ -71,6 +75,7 @@ function pilihOlahan(){
    document.getElementById("produk_dipilih").innerHTML = `
    Produk Olahan
    `;
+   databaseDipilih = 2;
 
    // Baca jumlah data
    jLenght = Object.keys(database_olahan).length;
@@ -79,17 +84,17 @@ function pilihOlahan(){
    document.getElementById("daftar_produk").innerHTML = ``;
 
    // Buat kolom berdasarkan jumlah data
-   for(let i = 1; i <= jLenght; i++)
+   for(let i = 0; i < jLenght; i++)
    {
-       idName = "produk_" + String(i);
+       idName = String(i);
 
        document.getElementById("daftar_produk").innerHTML += `
        <div class="col">
            <div class="card h-100 shadow">
-               <img src="${database_olahan[i-1].img}" class="card-img-top" alt="produk">
+               <img src="${database_olahan[i].img}" class="card-img-top" alt="produk">
                <div class="card-body" style="text-align:center">
-                   <h5 class="card-title" style="padding-bottom:20px">${database_olahan[i-1].nama_produk}</h5>
-                   <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="">Pilih</button>
+                   <h5 class="card-title" style="padding-bottom:20px">${database_olahan[i].nama_produk}</h5>
+                   <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="klikProduk(this.id)">Pilih</button>
                </div>
            </div>
        </div>
@@ -102,6 +107,7 @@ function pilihWisata(){
    document.getElementById("produk_dipilih").innerHTML = `
    Destinasi Wisata
    `;
+   databaseDipilih = 3;
 
    // Baca jumlah data
    jLenght = Object.keys(database_wisata).length;
@@ -110,20 +116,87 @@ function pilihWisata(){
    document.getElementById("daftar_produk").innerHTML = ``;
 
    // Buat kolom berdasarkan jumlah data
-   for(let i = 1; i <= jLenght; i++)
+   for(let i = 0; i < jLenght; i++)
    {
-       idName = "produk_" + String(i);
+       idName = String(i);
 
        document.getElementById("daftar_produk").innerHTML += `
        <div class="col">
            <div class="card h-100 shadow">
-               <img src="${database_wisata[i-1].img}" class="card-img-top" alt="produk">
+               <img src="${database_wisata[i].img}" class="card-img-top" alt="produk">
                <div class="card-body" style="text-align:center">
-                   <h5 class="card-title" style="padding-bottom:20px">${database_wisata[i-1].nama_produk}</h5>
-                   <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="">Pilih</button>
+                   <h5 class="card-title" style="padding-bottom:20px">${database_wisata[i].nama_produk}</h5>
+                   <button id="${idName}" type="button" class="btn btn-outline-success pilih-item" onclick="klikProduk(this.id)">Pilih</button>
                </div>
            </div>
        </div>
        `;
    };
+}
+
+// Untuk mendapatkan ID Produk yang dipilih
+function klikProduk(idProduk){
+    produkDipilih = idProduk;
+    showMap(databaseDipilih, produkDipilih);
+}
+
+
+// Untuk menampilkan peta lokasi
+function showMap(jenisDatabase, kodeProduk){
+    if(jenisDatabase == 0){
+        // Hapus Map Sebelumnya
+        document.getElementById("peta_lokasi").innerHTML = ``;
+        document.getElementById("peta_lokasi").innerHTML += `
+        <div class="map-responsive">
+            <iframe width="500" height="300" id="gmap_canvas" src="${database_pertanian[kodeProduk].lokasi}" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen></iframe>
+        </div>
+        `;
+        // Tampilkan nama lokasi
+        document.getElementById("nama_lokasi").innerHTML = ``;
+        document.getElementById("nama_lokasi").innerHTML += `
+        ${database_pertanian[kodeProduk].nama_produk}
+        `;
+    }
+    if(jenisDatabase == 1){
+        // Hapus Map Sebelumnya
+        document.getElementById("peta_lokasi").innerHTML = ``;
+        document.getElementById("peta_lokasi").innerHTML += `
+        <div class="map-responsive">
+            <iframe width="500" height="300" id="gmap_canvas" src="${database_peternakan[kodeProduk].lokasi}" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen></iframe>
+        </div>
+        `;
+        // Tampilkan nama lokasi
+        document.getElementById("nama_lokasi").innerHTML = ``;
+        document.getElementById("nama_lokasi").innerHTML += `
+        ${database_peternakan[kodeProduk].nama_produk}
+        `;
+    }
+    if(jenisDatabase == 2){
+        // Hapus Map Sebelumnya
+        document.getElementById("peta_lokasi").innerHTML = ``;
+        document.getElementById("peta_lokasi").innerHTML += `
+        <div class="map-responsive">
+            <iframe width="500" height="300" id="gmap_canvas" src="${database_olahan[kodeProduk].lokasi}" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen></iframe>
+        </div>
+        `;
+        // Tampilkan nama lokasi
+        document.getElementById("nama_lokasi").innerHTML = ``;
+        document.getElementById("nama_lokasi").innerHTML += `
+        ${database_olahan[kodeProduk].nama_produk}
+        `;
+    }
+    if(jenisDatabase == 3){
+        // Hapus Map Sebelumnya
+        document.getElementById("peta_lokasi").innerHTML = ``;
+        document.getElementById("peta_lokasi").innerHTML += `
+        <div class="map-responsive">
+            <iframe width="500" height="300" id="gmap_canvas" src="${database_wisata[kodeProduk].lokasi}" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen></iframe>
+        </div>
+        `;
+        // Tampilkan nama lokasi
+        document.getElementById("nama_lokasi").innerHTML = ``;
+        document.getElementById("nama_lokasi").innerHTML += `
+        ${database_wisata[kodeProduk].nama_produk}
+        `;
+    }
 }
