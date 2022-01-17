1c2db1b2765d4fa58626492734dbea71ce40fa54
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-17 19:04:41 +0700

    database umkm

---
+++ assets/database/database_pertanian.js
@@ -0,0 +1,32 @@
+database_pertanian = [
+    {
+        "nama_produk" : "Produk Tani Pak Joko",
+        "img" : "./assets/img/produkdesa/tani_1.jpeg",
+        "lokasi" : "https://maps.google.com/maps?q=Jetis%20Lor&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    },
+    {
+        "nama_produk" : "Produk Tani Bu Harti",
+        "img" : "./assets/img/produkdesa/tani_1.jpeg",
+        "lokasi" : "https://maps.google.com/maps?q=Jetis%20Lor&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    },
+    {
+        "nama_produk" : "Produk Tani Pak Suparmin",
+        "img" : "./assets/img/produkdesa/tani_1.jpeg",
+        "lokasi" : "https://maps.google.com/maps?q=Jetis%20Lor&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    },
+    {
+        "nama_produk" : "Produk Tani Mas Anto",
+        "img" : "./assets/img/produkdesa/tani_1.jpeg",
+        "lokasi" : "https://maps.google.com/maps?q=Jetis%20Lor&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    },
+    {
+        "nama_produk" : "Produk Tani Mbah Jasripan",
+        "img" : "./assets/img/produkdesa/tani_1.jpeg",
+        "lokasi" : "https://maps.google.com/maps?q=Jetis%20Lor&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    },
+    {
+        "nama_produk" : "Jeruk Manis Mas Prapto",
+        "img" : "./assets/img/produkdesa/tani_1.jpeg",
+        "lokasi" : "https://maps.google.com/maps?q=Jetis%20Lor&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    },
+  ];
