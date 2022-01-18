ca04addf9bd772be77becfa4c8a08569d2cebd3e
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-18 08:34:21 +0700

    daftar kategori

---
+++ database/database_kategori.js
@@ -0,0 +1,22 @@
+database_kategori = [
+    {
+        "idKategori" : 0,
+        "nama_kategori" : "Produk Pertanian",
+        "referensi" : database_pertanian,
+    },
+    {
+        "idKategori" : 1,
+        "nama_kategori" : "Produk Peternakan",
+        "referensi" : database_peternakan,
+    },
+    {
+        "idKategori" : 2,
+        "nama_kategori" : "Produk Olahan",
+        "referensi" : database_olahan,
+    },
+    {
+        "idKategori" : 3,
+        "nama_kategori" : "Destinasi Wisata",
+        "referensi" : database_wisata,
+    },
+  ];
