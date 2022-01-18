db1ee40c745b7d5be46f7198962301232a94c90e
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-18 10:12:29 +0700

    alamat kantor desa

--- assets/js/kontak.js
+++ assets/js/kontak.js
@@ -18,7 +18,7 @@ document.getElementById("kontak").innerHTML = `
         <!-- Google Maps -->
         <div class="col-lg" style="text-align:right; padding-bottom:20px">
             <div class="map-responsive">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63218.54571692121!2d111.182067!3d-7.982499000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e797eb6698fa427%3A0xa0ae8f1d85b10b87!2sJetis%20Lor%2C%20Nawangan%2C%20Pacitan%20Regency%2C%20East%20Java%2C%20Indonesia!5e0!3m2!1sen!2sus!4v1642473236051!5m2!1sen!2sus" width="500" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>            
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.7763767074797!2d111.18368912915516!3d-7.988039871548041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e797eb233fb0001%3A0x474058d696e932c!2sKantor%20desa%20jetis%20lor!5e0!3m2!1sen!2sus!4v1642475252332!5m2!1sen!2sus"" width="500" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>            
             </div>
         </div>
 
