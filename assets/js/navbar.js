1c96727c7202dfb7c1c677e504933456bdbdae83
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-18 17:39:05 +0700

    universal navbar

---
+++ assets/js/navbar.js
@@ -0,0 +1,33 @@
+document.getElementById("mainNav").innerHTML = `
+<div class="container-fluid">
+    <a class="navbar-brand" href="./index.html" >
+        <img src="./assets/img/logopacitan.png" alt="" width="auto" height="26" class="d-inline-block align-text-top">
+        <Strong>Desa Jetis Lor</Strong>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end navbar-menu" id="navbarSupportedContent" >
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="./profildesa.html" style="color:#ffffff">Profil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./produkdesa.html" style="color:#ffffff">Produk UKM</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./beritadesa.html" style="color:#ffffff">Berita</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./pemerintahan.html" style="color:#ffffff">Pemerintahan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#kontak" style="color:#ffffff">Kontak</a>
+            </li>
+            
+        </ul>
+    </div>
+</div>
+`;
