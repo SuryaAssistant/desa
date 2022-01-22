909dadc97f7e59c7ceaa2c2097aef46fa8149306
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-22 20:26:17 +0700

    try blog age

---
+++ berita/contoh-postingan.js
@@ -0,0 +1,67 @@
+// Deskripsi Postingan
+deskripsi = [
+    {
+        "judul" : "Contoh Postingan",
+        "penulis" : "Penulis Anonim",
+        "tanggal" : "18 Januari 2021",
+        "jam" : "18.00 WIB",
+        "deskripsi" : "Contoh template postingan html untuk website @2022",
+        "keywords" : "desa jetis lor, jetis lor, jetis, nawangan, pacitan",
+    },
+];
+
+
+// Isi Postingan
+document.getElementById('isiPost').innerHTML=`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Quisque porttitor dolor eleifend velit dignissim, nec vestibulum augue egestas. 
+Vestibulum tincidunt ante at nibh iaculis lacinia. Aliquam vel iaculis orci, pulvinar dictum sem. 
+Nullam at ligula sit amet sapien dapibus semper. 
+Pellentesque gravida tortor ut sagittis molestie. Nam vitae odio lorem. 
+Pellentesque malesuada lorem ac metus tempor, sit amet porta magna congue. 
+Praesent tempor id urna eget eleifend. Maecenas mattis, neque nec vestibulum sagittis, 
+mi dolor laoreet nunc, non rutrum quam justo id dui. Vivamus nisi dui, rutrum at dui sed, feugiat porttitor diam.
+<br>
+<br>
+Mauris varius sem in ante hendrerit efficitur. 
+Maecenas aliquet eros in dui luctus feugiat. Ut in neque ipsum. 
+Donec consequat sem sed eros fringilla iaculis. Nulla diam mi, congue eget vulputate et, aliquet sed odio. 
+Sed dapibus ex nec imperdiet venenatis. 
+Aenean rhoncus, mi quis eleifend convallis, sem ligula aliquet lectus, eu dignissim lacus nulla et ligula. 
+Nunc et velit sit amet urna dapibus euismod sit amet vestibulum tellus. Curabitur bibendum non augue ut tempus.
+<br>
+<br>
+Cras sem purus, consequat eu condimentum a, finibus a ipsum. 
+Etiam molestie tortor sit amet felis lacinia elementum. Donec ut massa metus. 
+Maecenas hendrerit sem in lectus mollis interdum. 
+Aenean ac bibendum justo, quis dignissim enim. In dui elit, cursus sed vulputate eget, consequat non elit. 
+Ut ut tellus at tellus eleifend rhoncus. In dictum mauris est, eu finibus tortor efficitur eget. 
+Cras finibus ac nunc quis imperdiet. Nulla suscipit dolor sed magna vulputate pellentesque. 
+Duis a magna tincidunt, commodo eros et, blandit orci. 
+Pellentesque lacus lectus, tempor ac tincidunt quis, sodales rhoncus justo. 
+Sed condimentum ante risus, interdum euismod enim egestas eget. 
+Proin rutrum, erat sodales faucibus venenatis, metus nunc sollicitudin quam, eget tempus libero lorem et enim. 
+Phasellus tortor mi, cursus ut sodales quis, dignissim id massa. Duis in nisi sit amet urna tristique aliquam.
+<br>
+<br>
+Nulla imperdiet vel massa non fringilla. 
+Cras egestas accumsan malesuada. 
+Integer sollicitudin orci ex, sed porttitor ligula accumsan eu. Vivamus nec enim dolor. 
+Praesent molestie odio at mauris pretium, eu consequat erat sagittis. 
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+Etiam in porttitor magna. Nulla facilisi.
+<br>
+Vestibulum sapien dui, tincidunt sit amet porta at, finibus ac leo. 
+Fusce sollicitudin tincidunt orci. Nulla facilisi. Mauris ut libero purus. 
+Pellentesque et turpis a magna eleifend dignissim. Nulla et pharetra nisi. 
+Proin id laoreet nisl, sed ornare massa. 
+Suspendisse mattis, arcu at consectetur cursus, est elit pretium sapien, et viverra elit lectus in elit. 
+Fusce sollicitudin vulputate sodales. Nam eget sodales nisl. 
+Vivamus bibendum accumsan ex, vel elementum felis fringilla a. 
+Vivamus sodales urna sit amet odio faucibus eleifend. Ut sapien sem, aliquet sit amet leo non, auctor maximus orci. 
+Pellentesque imperdiet vitae urna non vehicula. Quisque ultrices justo justo, sed dictum nunc faucibus cursus. 
+Nunc vehicula leo sed neque rhoncus ullamcorper.
+<br>
+<br>
+`;
+
