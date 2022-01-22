d8308d46ee1b9a20ae8813bba95f22026448a308
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-22 20:27:07 +0700

    change olahan to kerajinan

---
+++ database/database_kerajinan.js
@@ -0,0 +1,8 @@
+database_kerajinan = [
+    {
+        "nama_produk" : "Kerajian Tangan Rahmat Craft",
+        "img" : "produk_1.png",
+        "kontak" : "081234567890",
+        "lokasi" : "https://maps.google.com/maps?q=Jetis%20Lor&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    },
+  ];
