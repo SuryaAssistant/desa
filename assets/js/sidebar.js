d3187b7452aeee0295e1e17822de172ea60d1e4c
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-22 20:25:37 +0700

    blog sidebar

---
+++ assets/js/sidebar.js
@@ -0,0 +1,16 @@
+// Cari artikel
+document.getElementById("cari_artikel").innerHTML = `
+
+`;
+
+// Informasi Umum
+document.getElementById("info_desa").innerHTML = `
+<p align="left" style="background-color:#F1F1F1"><Strong>Info Desa</Strong></p>
+<img src="./assets/img/logopacitan.png" align="center" width="200px" style="padding-bottom:20px">
+`;
+
+// Postingan Lainnya
+document.getElementById("artikel_lain").innerHTML = `
+<p align="left" style="background-color:#F1F1F1"><Strong>Postingan Lainnya</Strong></p>
+
+`;
