f5fe6543435aacf016c69444b1e84f7abbc65141
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-24 20:19:27 +0700

    event remainder

--- assets/js/nav_transparent.js
+++ assets/js/nav_transparent.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
           
     $(window).scroll(function(){
 
+      var element = document.getElementById("alertNav");
       if($(window).scrollTop()>300){
         $('nav').addClass('navtop');
+        element.classList.add("alertColor");
       }else{
         $('nav').removeClass('navtop');
+        element.classList.remove("alertColor");
       }
 
     });
