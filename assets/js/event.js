036c6b7a63e6f14d7ccc085a4b111b856fac9e6e
SuryaAssistant <09.fandisurya@gmail.com>
2022-01-25 11:12:59 +0700

    event database

---
+++ assets/js/event.js
@@ -0,0 +1,41 @@
+/* ----------- Buat daftar event terdekat -------------- */
+
+// baca database_event
+var jumlahEvent = Object.keys(database_event).length;
+
+// buat database baru untuk event terdekat
+eventTerdekat = [
+    {
+    },
+];
+
+const d = new Date();
+
+var eventTerdekatNumber = 0;
+let day = d.getDate();
+var tanggalDipilih = day;
+let month = d.getMonth();
+var bulanDipilih = month;
+let year = d.getFullYear();
+var tahunDipilih = year;
+
+
+for(var i=0; i<jumlahEvent; i++){
+    if(database_event[i].tahun >= tahunDipilih && database_event[i].bulan >= bulanDipilih+1 && database_event[i].tanggal >= tanggalDipilih){
+        eventTerdekat[eventTerdekatNumber] = database_event[i];
+        eventTerdekatNumber += 1;
+    }
+}
+
+
+// Convert bulan
+var bulan = ["Januari", "Februari", 'Maret', "April", "Mei", "Juni", "Juli", 
+    "Agustus", "September", "Oktober", "November", "Desember"];
+
+
+// Tampilkan ke index.html
+document.getElementById("event_desa").innerHTML = `
+<p class="text-white" style="padding-top:70px; font-size:12px; margin-bottom:0px">
+    ${eventTerdekat[0].nama_kegiatan} : ${eventTerdekat[0].hari}, ${eventTerdekat[0].tanggal} ${bulan[eventTerdekat[0].bulan - 1]} ${eventTerdekat[0].tahun}
+</p>
+`;
--- database/database_event.js
+++ database/database_event.js
@@ -1,9 +1,23 @@
 database_event = [
     {
         "nama_kegiatan" : "Kerja Bakti",
-        "kepada" : "masyarakat Desa Jetis lor",
-        "tanggal" : 12,
-        "bulan" : 3,
-        "tahun" : 2021,
+        "hari" : "Senin",
+        "tanggal" : 25,
+        "bulan" : 1,
+        "tahun" : 2022,
+    },
+    {
+        "nama_kegiatan" : "Kerja Bakti",
+        "hari" : "Senin",
+        "tanggal" : 24,
+        "bulan" : 1,
+        "tahun" : 2022,
+    },
+    {
+        "nama_kegiatan" : "NoneBakti",
+        "hari" : "Senin",
+        "tanggal" : 26,
+        "bulan" : 2,
+        "tahun" : 2022,
     },
   ];
