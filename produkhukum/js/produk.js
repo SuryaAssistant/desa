273517432fe0eee72f54940044ce85bd64d332cb
SuryaAssistant <09.fandisurya@gmail.com>
2022-02-02 06:49:55 +0700

    book logo

--- produkhukum/js/produk.js
+++ produkhukum/js/produk.js
@@ -18,7 +18,7 @@ function displayKategori(){
 
     // Hapus data sebelumnya
     document.getElementById("kategori_produk").innerHTML = `
-    <div id="chooseKategori" class="row row-cols-1 row-cols-3 g-2" style="margin-left:10%; margin-right:10%;" align="center">
+    <div id="chooseKategori" class="row row-cols-1 row-cols-md-4 g-4" style="margin-left:10%; margin-right:10%;" align="center">
     </div>
     `;
 
@@ -28,6 +28,9 @@ function displayKategori(){
         <div class="col">
             <div id="${i}" class="card shadow h-100" onclick="pilihKategori(this.id)">
                 <div class="card-body" style="padding-top:70px; padding-bottom:70px">
+                    <i class='bx bxs-book' style="font-size:40px"></i>
+                    <br>
+                    <br>
                     <h5 class="card-title" style="font-size:14px">${kategoriProdukHukum[i].kategori_produk_hukum}</h5>
                 </div>
             </div>
