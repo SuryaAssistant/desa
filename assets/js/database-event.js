8de891ac0ad175b4edf77057edc46856d0602e84
SuryaAssistant <09.fandisurya@gmail.com>
2022-02-04 19:24:08 +0700

    supabase database

---
+++ assets/js/database-event.js
@@ -0,0 +1,18 @@
+// Import Database
+
+const { createClient } = supabase
+const _supabase = createClient('https://xnqzrkafpdwivpjtwdvr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY2ODY1NywiZXhwIjoxOTU5MjQ0NjU3fQ.pkB-RXyZOKGvNeEHbGEjoD9KDeFV_5i4UFiIO7L_--g')
+
+// Read database and save as database
+async function getData() {
+    let { data, error } = await _supabase.from('event').select('*')
+    return data
+  }
+  
+  getData().then((data) => { 
+    database_event = data;
+    console.log(database_event)
+    showEvent();
+  }
+);
+
--- assets/js/event.js
+++ assets/js/event.js
@@ -1,7 +1,6 @@
 /* ----------- Buat daftar event terdekat -------------- */
 
-// baca database_event
-var jumlahEvent = Object.keys(database_event).length;
+var database_event = [{}];
 
 // buat database baru untuk event terdekat
 eventTerdekat = [
@@ -20,25 +19,39 @@ let year = d.getFullYear();
 var tahunDipilih = year;
 
 
-for(var i=0; i<jumlahEvent; i++){
-    if(database_event[i].tahun >= tahunDipilih && database_event[i].bulan >= bulanDipilih+1 && database_event[i].tanggal >= tanggalDipilih){
-        eventTerdekat[eventTerdekatNumber] = database_event[i];
-        eventTerdekatNumber += 1;
+var tanggalEvent;
+var bulanEvent;
+var tahunEvent;
+
+
+
+function showEvent(){
+    // baca database_event
+    var jumlahEvent = Object.keys(database_event).length;
+
+    for(var i=0; i<jumlahEvent; i++){
+        tahunEvent = parseInt(database_event[i].date.substr(0,4));
+        bulanEvent = parseInt(database_event[i].date.substr(5,2));
+        tanggalEvent = parseInt(database_event[i].date.substr(8,2));
+
+        if(tahunEvent >= tahunDipilih && bulanEvent >= bulanDipilih+1 && tanggalEvent >= tanggalDipilih){
+            eventTerdekat[eventTerdekatNumber] = database_event[i];
+            eventTerdekatNumber += 1;
+        }
     }
-}
 
 
-// Convert bulan
-var bulan = ["Januari", "Februari", 'Maret', "April", "Mei", "Juni", "Juli", 
-    "Agustus", "September", "Oktober", "November", "Desember"];
+    // Convert bulan
+    var bulan = ["Januari", "Februari", 'Maret', "April", "Mei", "Juni", "Juli", 
+        "Agustus", "September", "Oktober", "November", "Desember"];
 
 
-// Tampilkan ke index.html
-
-if(eventTerdekat[0].nama_kegiatan != undefined){
-    document.getElementById("event_desa").innerHTML = `
-    <p class="text-white" style="padding-top:70px; font-size:12px; margin-bottom:0px">
-        ${eventTerdekat[0].nama_kegiatan} : ${eventTerdekat[0].hari}, ${eventTerdekat[0].tanggal} ${bulan[eventTerdekat[0].bulan - 1]} ${eventTerdekat[0].tahun}
-    </p>
-    `;
+    // Tampilkan ke index.html
+    if(eventTerdekat[0].nama_kegiatan != undefined){
+        document.getElementById("event_desa").innerHTML = `
+        <p class="text-white" style="padding-top:70px; font-size:12px; margin-bottom:0px">
+            ${eventTerdekat[0].nama_kegiatan} : ${eventTerdekat[0].date.substr(8,2)} ${bulan[parseInt(eventTerdekat[0].date.substr(5,2)) - 1]} ${eventTerdekat[0].date.substr(0,4)}
+        </p>
+        `;
+    };
 };
