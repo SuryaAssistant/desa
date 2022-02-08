db16fddc7b190389ceb17bac262d543c6f4dd495
SuryaAssistant <09.fandisurya@gmail.com>
2022-02-08 17:16:22 +0700

    intro

--- berita/assets/js/posting.js
+++ berita/assets/js/posting.js
@@ -57,15 +57,14 @@ function update_artikel(){
     // Share Post
     document.getElementById("sharePost").innerHTML = `
     <p><Strong>Share</Strong> </p>
-
-    <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-    <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
-    <a class="a2a_button_facebook"></a>
-    <a class="a2a_button_twitter"></a>
-    <a class="a2a_button_email"></a>
-    <a class="a2a_button_whatsapp"></a>
-    <a class="a2a_button_telegram"></a>
-    <a class="a2a_button_linkedin"></a>
+        <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+        <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+        <a class="a2a_button_facebook"></a>
+        <a class="a2a_button_twitter"></a>
+        <a class="a2a_button_email"></a>
+        <a class="a2a_button_whatsapp"></a>
+        <a class="a2a_button_telegram"></a>
+        <a class="a2a_button_linkedin"></a>
     </div>
     `;
 
